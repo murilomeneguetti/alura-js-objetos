e96b5a145d06719e5531e5a050e17db27e6a56d8
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-11-24 17:41:12 -0300

    aula 1

---
+++ aula1/acesso_colchete.js
@@ -0,0 +1,18 @@
+const cliente = {
+    nome: 'Andre',
+    idade: 23,
+    cpf: '123456789',
+    email: 'andre@dominio.com'
+};
+
+console.log(`O nome do cliente é ${cliente['cpf']} e essa pessoa tem ${cliente['idade']} anos.`);
+
+console.log(`O 3 primeiros digitos do CPF são ${cliente.cpf.substring(0, 3)}`);
+
+//situação em que é preciso usar o [] para acessar o valor do objeto:
+
+const chaves = ['nome', 'idade', 'cpf', 'email', 'altura'];
+
+chaves.forEach((chave) => {
+    console.log(`A chave ${chave} tem valor ${cliente[chave]}`)
+});
