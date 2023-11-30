13b9dc7094ef6250f12a82fdff5a25acbe55e557
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-11-30 14:16:32 -0300

    Create teste_array.js

---
+++ aula1/teste_array.js
@@ -0,0 +1,15 @@
+// Criar um array vazio com 10 posições
+let meuArray = new Array(10);
+
+// Verificar se a quarta posição existe (índice 3)
+if (meuArray.length > 3) {
+  // Preencher somente a quarta posição
+  meuArray[3] = "Valor na quarta posição";
+}
+
+// Exibir o array
+console.log(meuArray);
+
+meuArray[4] = 'teste';
+
+console.log(meuArray);
