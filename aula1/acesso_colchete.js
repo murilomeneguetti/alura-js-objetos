1267c094294a0d9a9186d39b622599246decf06f
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-12-14 13:30:42 -0300

    correção no nome do campo

--- aula1/acesso_colchete.js
+++ aula1/acesso_colchete.js
@@ -5,7 +5,7 @@ const cliente = {
     email: 'andre@dominio.com'
 };
 
-console.log(`O nome do cliente é ${cliente['cpf']} e essa pessoa tem ${cliente['idade']} anos.`);
+console.log(`O nome do cliente é ${cliente['nome']} e essa pessoa tem ${cliente['idade']} anos.`);
 
 console.log(`O 3 primeiros digitos do CPF são ${cliente.cpf.substring(0, 3)}`);
 
